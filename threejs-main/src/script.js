fa80071993e241a56bde84eb3da4cd40fb47a16f
Anna Stefanova <astefanova267@gmail.com>
2023-11-21 17:33:28 +0300

    OrbitControls - autoRotate, enableDamping, autoRotateSpeed

--- threejs-main/src/script.js
+++ threejs-main/src/script.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import './style.css';
+import {func} from "three/addons/nodes/code/FunctionNode";
 
 const scene = new THREE.Scene();
 const axesHelper = new THREE.AxesHelper(3);
@@ -8,38 +10,11 @@ scene.add(axesHelper);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({
-    color: 'purple',
+    color: 'yellow',
     wireframe: true
 });
 
-// const group = new THREE.Group();
-// group.scale.x = 2;
-// group.rotation.x = Math.PI * 0.25;
-//
-// const cube1 = new THREE.Mesh(geometry, material);
-// cube1.position.x = -1.5;
-//
-// const cube2 = new THREE.Mesh(geometry, material);
-// cube2.position.x = 0;
-//
-// const cube3 = new THREE.Mesh(geometry, material);
-// cube3.position.x = 1.5;
-//
-// group.add(cube1, cube2, cube3);
-
-// scene.add(group);
-
 const mesh = new THREE.Mesh(geometry, material);
-// mesh.position.y = 0.8;
-// mesh.position.x = 1.5;
-// mesh.position.z = 1.5;
-//
-// mesh.scale.x = 2;
-// mesh.scale.y = 0.8;
-// mesh.scale.z = 0.5;
-//
-// mesh.rotation.x = Math.PI * 0.25;
-// mesh.rotation.x = Math.PI * 0.75;
 
 scene.add(mesh);
 
@@ -48,13 +23,19 @@ const sizes = {
     height: 600
 }
 
+const canvas = document.querySelector('.canvas');
+
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.y = 1;
 camera.position.z = 3;
-// camera.position.y =1;
+
+const controls = new OrbitControls(camera, canvas);
+
+controls.enableDamping = true;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 15;
 
 scene.add(camera);
-
-const canvas = document.querySelector('.canvas2');
 
 
 const renderer = new THREE.WebGLRenderer({canvas});
@@ -65,17 +46,12 @@ renderer.render(scene, camera);
 const clock = new THREE.Clock();
 
 const tick = () => {
-    const elapsedTime = clock.getElapsedTime();
+    const delta = clock.getDelta();
 
-    camera.position.x = Math.cos(elapsedTime);
-    camera.position.y = Math.sin(elapsedTime);
-    camera.lookAt(mesh.position);
-
+    controls.update(delta);
     renderer.render(scene, camera);
     window.requestAnimationFrame(tick);
-};
+}
+
 tick();
-
-
-
 
